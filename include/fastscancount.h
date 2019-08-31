0a5b6c889dc5146f46db7268853682585fe9a8b8
Daniel Lemire <lemire@gmail.com>
2019-08-31 11:33:41 -0400

    What if we were to decrement instead?

--- include/fastscancount.h
+++ include/fastscancount.h
@@ -16,15 +16,15 @@ namespace {
 // used by natefastscancount
 uint32_t *natefastscancount_maincheck(uint8_t *counters, size_t &it,
                                       const uint32_t *d, size_t start,
-                                      size_t range, uint8_t threshold,
+                                      size_t range, 
                                       uint32_t *out) {
   range += start;
   counters -= start;
   size_t i = it;
   for (uint32_t val = d[i]; val < range; val = d[++i]) {
     uint8_t c = counters[val];
-    if (c == threshold) *out++ = val;
-    counters[val] = c + 1;
+    if (c == 0) *out++ = val;
+    counters[val] = c - 1;
   }
   it = i;
   return out;
@@ -33,20 +33,18 @@ uint32_t *natefastscancount_maincheck(uint8_t *counters, size_t &it,
 // used by natefastscancount
 uint32_t *natefastscancount_finalcheck(uint8_t *counters, size_t &it,
                                        const uint32_t *d, size_t start,
-                                       size_t itend, size_t threshold,
+                                       size_t itend, 
                                        uint32_t *out) {
-  uint8_t t = threshold + 1;
   uint8_t *const deccounters = counters - start;
   size_t i = it;
   for (; i < itend; i++) {
     uint32_t val = d[i];
     uint8_t *location = deccounters + val;
     uint8_t c = *location;
-    c++;
-    *location = c;
-    if (c == t) {
+    if (c == 0) {
       *out++ = val;
     }
+    *location = c - 1;
   }
   it = i;
   return out;
@@ -78,7 +76,7 @@ void fastscancount(std::vector<std::vector<uint32_t>> &data,
       initout = out.data();
       output = out.data() + countsofar;
     }
-    memset(counters.data(), 0, range);
+    memset(counters.data(), threshold, range);
     for (size_t c = 0; c < ds; c++) {
       size_t it = iters[c]; // recover where we were
       std::vector<uint32_t> &d = data[c];
@@ -89,10 +87,10 @@ void fastscancount(std::vector<std::vector<uint32_t>> &data,
       bool near_the_end = (d[itend - 1] < start + range);
       if (near_the_end) {
         output = natefastscancount_finalcheck(counters.data(), it, d.data(),
-                                              start, itend, threshold, output);
+                                              start, itend, output);
       } else {
         output = natefastscancount_maincheck(counters.data(), it, d.data(),
-                                             start, range, threshold, output);
+                                             start, range, output);
       }
       iters[c] = it; // store it for next round
     }
